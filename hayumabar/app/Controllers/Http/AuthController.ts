407005831fbcbb8c826bf8e8e167466c30d2f1cd
muhfauziazhar <muhammad.fauzi73@ui.ac.id>
2022-04-02 07:12:11 +0700

    sent otp to an email success register

--- hayumabar/app/Controllers/Http/AuthController.ts
+++ hayumabar/app/Controllers/Http/AuthController.ts
@@ -2,14 +2,23 @@ import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import { schema, rules } from "@ioc:Adonis/Core/Validator";
 import User from "App/Models/User";
 import UserValidator from "App/Validators/UserValidator";
+import Mail from "@ioc:Adonis/Addons/Mail";
 
 export default class AuthController {
   public async register({ request, response }: HttpContextContract) {
     try {
       const data = await request.validate(UserValidator);
       const newUser = await User.create(data);
+      const otp_code = Math.floor(100000 + Math.random() * 900000);
+      await Mail.send((message) => {
+        message
+          .from("adonis.demo@sanberdev.com")
+          .to(data.email)
+          .subject("Welcome Onboard!")
+          .htmlView("emails/otp_verification", { otp_code });
+      });
       response.created({
-        message: "Berhasil membuat user/owner baru",
+        message: "Register success, please verify your otp code in your email",
         data: newUser.name,
       });
     } catch (error) {
